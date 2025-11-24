7bb14d388978769f1c602be037a5d710b1973dac
Edson-Gmr <edson.guimaraes01@gmail.com>
2025-11-23 21:48:02 -0300

    Aula 3
    Variações formatação de string.

---
+++ 3string_tipo.py
@@ -0,0 +1,23 @@
+"""" (DocString)
+Python = linguagem de programação
+Tipo de tipagem = Dinâminca / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+print(1234)
+
+# Aspas simples
+print('Edson Junior')
+
+# Aspas dupla
+print("Edson Junior")
+
+# Escape
+print("Edson \"Junior\"")
+
+# -> Variantes na escrita:
+print("1, Edson 'Junior'")
+print('2, Edson "Junior"')
+
+# r
+print(r"Edson \"Junior\"")
